56ef61ebf2f72714d76770cfb2870e6fff6e131a
Bitcurrant <80823077+Bitcurrant@users.noreply.github.com>
2022-01-22 09:13:15 +0100

    update xps.js

--- scripts/xps.js
+++ scripts/xps.js
@@ -33,7 +33,7 @@ function CreateTable(inData) {
 
     // ADD JSON DATA TO THE TABLE AS ROWS.
     // DEBUG
-    console.log("land cnt" : data.length)
+    console.log("land cnt :" + data.length);
     for (var i = 0; i < data.length; i++) {
         // DEBUG
         //if (data[i].owner =='mzzra.wam') 
